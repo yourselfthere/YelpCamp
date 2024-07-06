fff24f4116ee6d514569ba66ee4f622f1152c7fa
Navneet Singh <nsd02rtk@gmail.com>
2024-07-07 03:22:38 +0530

    added new boilerplate.ejs and css etc.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+
+const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+
 const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -16,7 +19,7 @@ db.once("open", () => {
 });
 
 const app = express();
-
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
